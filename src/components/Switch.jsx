7ced5c7a1072e3d4321f60ec76585fff9f908ce5
Victor Matias <fvictor1705@gmail.com>
2022-11-14 23:21:05 -0300

    feat: add Switch component in the aplication

---
+++ src/components/Switch.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import '../css/Switch.css'
+
+function Switch() {
+  const [language, setLanguage] = useState(
+    { pt: 'ON', en: 'OFF', background: 'PT' },
+  );
+
+  return (
+    <section className='header-rigth-side FLEX__ROW__SPACE-EVENLY__CENTER'>
+      <p className={`header-switch-label ${language.pt}`}>PT-BR</p>
+        <div
+          className='header-switch-background'
+          onClick={ () => {
+            if (language.pt === 'ON') {
+              setLanguage({ pt: 'OFF', en: 'ON', background: 'EN' })
+            } else {
+              setLanguage({ pt: 'ON', en: 'OFF', background: 'PT' })
+            }
+          } }
+        >
+          <div className={`header-switch ${language.background}`}></div>
+        </div>
+        <p className={`header-switch-label ${language.en}`}>EN</p>
+    </section>
+  )
+}
+
+export default Switch
