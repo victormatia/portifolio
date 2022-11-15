3515f6e7bb35e5fabebdc4af9b62f100846bb9bd
Victor Matias <fvictor1705@gmail.com>
2022-11-15 00:18:56 -0300

    feat: start home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import './css/index.css'
+import Home from './pages/Home'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <section className='home-background'>
         <main>
           <Header />
+          <Home />
           <div>Este será o meu portifólio</div>
         </main>
       </section>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import hero from '../images/hero.png'
+import underlines from '../images/underlines.png'
+import '../css/Home.css'
+
+function Home() {
+  return (
+    <section className='home'>
+      <section className='home-left-side'>
+        <p className='home-title'><span>Olá</span> pessoas!</p>
+        <p className='home-paragraph'>Eu sou Victor Matias, mas pode me chamar de Vitu. <br/>
+        Sejam bem-vindos ao meu portifólio</p>
+        <img className='home-underlines' src={ underlines } alt="underlines" />
+      </section>
+
+      <section className='home-right-side'>
+        <img className='home-hero' src={ hero } alt="Foto de Victor Matias" />
+      </section>
+    </section>
+  )
+}
+
+export default Home
