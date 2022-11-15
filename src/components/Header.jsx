787edc2ef533974003b315034ec7fbeb4e50946d
Victor Matias <fvictor1705@gmail.com>
2022-11-14 23:22:28 -0300

    refactor: Header.jsx, Header.css and index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../images/VITU-logo.png';
 import '../css/Header.css'
+import Switch from '../components/Switch';
 
 function Header() {
-  const [language, setLanguage] = useState(
-    { pt: 'ON', en: 'OFF', background: 'PT' },
-  );
-
-  useEffect(() => {
-    console.log(language.pt, language.en);
-    console.log(language.background);
-  }, [language]);
-
   return (
     <header className='header FLEX__ROW__SPACE-EVENLY__CENTER'>
       <section className='header-left-side FLEX__ROW__SPACE-EVENLY__CENTER'>
@@ -22,23 +14,8 @@ function Header() {
           <li className='header-nav-link'><a href="">contato</a></li>
         </ul>
       </section>
-
-      <section className='header-rigth-side FLEX__ROW__SPACE-EVENLY__CENTER'>
-        <p className={`header-trigger-label ${language.pt}`}>PT-BR</p>
-        <div
-          className='header-trigger-background'
-          onClick={ () => {
-            if (language.pt === 'ON') {
-              setLanguage({ pt: 'OFF', en: 'ON', background: 'EN' })
-            } else {
-              setLanguage({ pt: 'ON', en: 'OFF', background: 'PT' })
-            }
-          } }
-        >
-          <div className={`header-trigger ${language.background}`}></div>
-        </div>
-        <p className={`header-trigger-label ${language.en}`}>EN</p>
-      </section>
+      
+      <Switch />
     </header>
   );
 }
