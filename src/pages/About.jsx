416f983bdb27f13f78744015c0aa08fd4d663ea8
Victor Matias <fvictor1705@gmail.com>
2022-11-20 02:19:29 -0300

    feat: start About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import hero from '../images/hero.png'
+
+function About() {
+  return (
+    <section id='about'>
+       About
+    </section>
+  )
+}
+
+export default About;
