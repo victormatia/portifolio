f1bb333d8b06824484e5e436477375b60f89b734
Victor Matias <fvictor1705@gmail.com>
2022-11-20 01:31:29 -0300

    refactor: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Header from './components/Header'
 import './css/index.css'
+import About from './pages/About'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <>
+    <main>
       <section className='home-background'>
-        <main>
-          <Header />
-          <Home />
-          <div>Este será o meu portifólio</div>
-        </main>
+        <Header />
+        <Home />
       </section>
-    </>
+      <section className='about-background'>
+        <About />
+      </section>
+    </main>
   )
 }
 
