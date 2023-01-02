2a468341f206d4ece3f187595ff07b3d7fe2080b
Victor Matias <fvictor1705@gmail.com>
2023-01-02 19:32:09 -0300

    feat: structure the card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../css/Card.css";
 import githubIcon from "../images/github-icon.png";
+import deployIcon from "../images/deploy-icon.png";
 
 function Card({ projectInfos: { name, repo, link } }) {
   return (
@@ -8,8 +9,8 @@ function Card({ projectInfos: { name, repo, link } }) {
       <img className='card-image' src="https://cdn.dribbble.com/userupload/4160565/file/original-f6d2c9b9fc324e41bbca071ed29a036a.png?compress=1&resize=752x564" alt="miniatura do projeto" />
       <h4 className='card-title' style={ { color: "black" } }>{ name }</h4>
       <section className='card-links-section'>
-        <a className='card-repo-link' href={ repo } target="_blank"><img className='card-icons' src={ githubIcon } alt="" /><p>Repositório</p></a>
-        <a className='card-deploy-link' href={ link } target="_blank"><p>Deployment</p></a>
+        <a className='card-repo-link' href={ repo } target="_blank"><img className='card-github-icon' src={ githubIcon } alt="" /><p>Repositório</p></a>
+        <a className='card-deploy-link' href={ link } target="_blank"><img className='card-deploy-icon' src={ deployIcon } alt="" /><p>Deployment</p></a>
       </section>
     </li>
   )
