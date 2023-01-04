ceb826124dc8204c8ca1df5c8ec91b230196a538
Victor Matias <fvictor1705@gmail.com>
2023-01-04 18:03:24 -0300

    feat: add logic in navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,37 @@ import About from './pages/About'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import './css/index.css'
+import { useRef, useEffect, useState } from 'react'
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+  const containerRef = useRef(null);
+
+  const options = {
+    root:  null,
+    rootMargin: '0px',
+    threshold: 0.95,
+  };
+  
+  const callback = (entries) => {
+    const [entrie] = entries;
+    const isVisible = entrie.isIntersecting;
+
+    setShowNav(!isVisible)
+
+  }
+  
+  const observer = new IntersectionObserver(callback, options);
+
+  useEffect(() => {
+    observer.observe(containerRef.current);
+  }, [containerRef])
+
   return (
     <main>
       <Header />
-      <Navbar />
-      <section id='home' className='home-background'>
+      { showNav && <Navbar /> }
+      <section ref={ containerRef } id='home' className='home-background'>
         <Home />
       </section>
       <section id='about' className='about-background'>
@@ -23,7 +47,5 @@ function App() {
     </main>
   )
 }
-
-// o id para a navegação é melhor posicionado na section de background de cada página
 
 export default App
