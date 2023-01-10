aae4eb3fe9493fd86ef4aec992c79fe7b93d9803
Victor Matias <fvictor1705@gmail.com>
2023-01-10 12:29:15 -0300

    feat: move DATA to mockApi.js

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,39 +1,14 @@
 import React from 'react';
+import { aboutMe } from "../mock/mockApi";
 import '../css/Timeline.css';
 
-const DATA = [
-  {
-    id: 1,
-    title: 'Descobri minha paixão por desenvolvimento de softwares',
-    context: 'Precisei de um site para a minha empresa e descidi construir por conta própria',
-    date: '2021/06',
-  },
-  {
-    id: 2,
-    title: 'Descidi me dedicar totalmente aos estudos de programação',
-    context: 'Optei por fazer transição de carreira  e iniciei o curso de desenvolvimento web na Trybe para me tornar um profissional Full Stack',
-    date: '2022/04',
-  },
-  {
-    id: 3,
-    title: 'Descidi me dedicar totalmente aos estudos de programação',
-    context: 'Optei por fazer transição de carreira  e iniciei o curso de desenvolvimento web na Trybe para me tornar um profissional Full Stack',
-    date: '2022/04',
-  },
-  {
-    id: 4,
-    title: 'Me tornei um profissional Front-end',
-    context: 'Finalizei o segundo módulo da Trybe, ganhei a certificação de profissional front-end e posso atuar no mercado',
-    date: '2022/06',
-  },
-  
-];
 
 function Timeline() {
+  const { timeLinePoints } = aboutMe;
   return (
     <section className="timeline">
       {
-        DATA.map(({
+        timeLinePoints.map(({
           id, title, context, date,
         }) => {
           let align = '';
