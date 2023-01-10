9537fd4ceaccff89f54ee7b5d253d87590f5c1a9
Victor Matias <fvictor1705@gmail.com>
2023-01-10 11:28:40 -0300

    feat: create a file for mocks

---
+++ src/mock/mockApi.js
@@ -0,0 +1,28 @@
+const mockProjects = [
+  {
+    name: 'Recipes',
+    tag: 'FrontEnd',
+    repo: 'https://github.com/victormatia/recipes-app',
+    link: 'https://recipes-hdxwcw7ku-victormatia.vercel.app/',
+  },
+  {
+    name: 'Wallet',
+    tag: 'FrontEnd',
+    repo: 'https://github.com/victormatia/wallet-redux',
+    link: 'https://wallet-redux-bh14u6cvy-victormatia.vercel.app/',
+  },
+  {
+    name: 'BlogsApi',
+    tag: 'BackEnd',
+    repo: 'https://github.com/victormatia/blogs-api',
+    link: '',
+  },
+  {
+    name: 'Trybe Tunes',
+    tag: 'Em breve',
+    repo: '',
+    link: '',
+  },
+];
+
+export default mockProjects;
