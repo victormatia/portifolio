d4668dc9f125bac93d5717f5060defc661bcd967
Victor Matias <fvictor1705@gmail.com>
2023-01-10 12:33:35 -0300

    feat: add description paragraph to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Timeline from '../components/Timeline';
+import { aboutMe } from "../mock/mockApi";
 import '../css/About.css';
 
 function About() {
+  const { about } = aboutMe;
   return (
     <section className='about'>
        <h1 className='about-title'>Sobre mim</h1>
+       <p className='about-description'>{ about }</p>
        <Timeline />
     </section>
   )
