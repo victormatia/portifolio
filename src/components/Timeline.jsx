578c9fccd8a7f95f735405dd4012ad0efd4734f8
Victor Matias <fvictor1705@gmail.com>
2023-02-20 19:10:52 -0300

    feat: restructure timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -11,46 +11,14 @@ function Timeline() {
         timeLinePoints.map(({
           id, title, context, date,
         }) => {
-          let align = '';
-
-          if (id % 2 === 0) {
-            align = 'timeline-point-left';
-
-            return (
-              <div key={id} className={`${align}`}>
-
-                <div className="date-section">
-                  <p className="point-date">{ date }</p>
-                </div>
-
-                <div className="bulletpoint-section">
-                  <div className="bulletpoint" />
-                </div>
-
-                <div className="title-context-section">
-                  <h3 className="point-title">{ title }</h3>
-                  <p className="point-context">{ context }</p>
-                </div>
-              </div>
-            );
-          }
-
-          align = 'timeline-point-right';
-
           return (
-            <div key={id} className={`${align}`}>
+            <div key={id} className='timeline-point'>
               <div className="title-context-section">
                 <h3 className="point-title">{ title }</h3>
                 <p className="point-context">{ context }</p>
               </div>
-
-              <div className="bulletpoint-section">
-                <div className="bulletpoint" />
-              </div>
-
-              <div className="date-section">
-                <p className="point-date">{ date }</p>
-              </div>
+              <p className="point-date">{ date }</p>
+              <div className="bulletpoint" />
             </div>
           );
         })
