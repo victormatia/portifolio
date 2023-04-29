b89b1b0a7c0a68247d6dc7e24c2dc37259df5f13
victormatia <fvictor1705@gmail.com>
2023-04-29 15:08:13 -0300

    refactor: update classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import logo from '../images/VITU-logo.png';
-import '../css/Header.css'
+import styles from '../css/modules/Header.module.css'
 import Switch from '../components/Switch';
 
 function Header() {
   return (
-    <header className='header FLEX__ROW__SPACE-BETWEEN__CENTER'>
-      <section className='header-left-side FLEX__ROW__SPACE-BETWEEN__CENTER'>
-        <img className='header-logo' src={ logo } alt="VITU logo" />
-        <ul className='header-nav'>
-          <li className='header-nav-link'><a href="#about">sobre</a></li>
-          <li className='header-nav-link'><a href="#projects">projetos</a></li>
-          <li className='header-nav-link'><a href="#contact">contato</a></li>
+    <header className={ styles['header'] }>
+      <section className={ styles['header__l-side'] }>
+        <img className={ styles['header__logo'] } src={ logo } alt="VITU logo" />
+        <ul className={ styles['header__links'] }>
+          <li><a href="#about">sobre</a></li>
+          <li><a href="#projects">projetos</a></li>
+          <li><a href="#contact">contato</a></li>
         </ul>
       </section>
-      
       <Switch />
     </header>
   );
