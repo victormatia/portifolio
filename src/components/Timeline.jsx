59bd7d56874c34af9b97cd479d31a86c9e5ac275
victormatia <fvictor1705@gmail.com>
2023-04-30 22:14:45 -0300

    refactor: remove code lines unnecessary and update classes

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,61 +1,26 @@
 import React from 'react';
 import { aboutMe } from "../mock/mockApi";
-import '../css/Timeline.css';
+import styles from '../css/modules/Timeline.module.css'
 
 
 function Timeline() {
   const { timeLinePoints } = aboutMe;
   return (
-    <section className="timeline">
+    <section className={ styles['timeline'] }>
       {
         timeLinePoints.map(({
           id, title, context, date,
-        }) => {
-          let align = '';
-
-          if (id % 2 === 0) {
-            align = 'timeline-point-left';
-
-            return (
-              <div key={id} className={`${align}`}>
-
-                <div className="date-section">
-                  <p className="point-date">{ date }</p>
-                </div>
-
-                <div className="bulletpoint-section">
-                  <div className="bulletpoint" />
-                </div>
-
-                <div className="title-context-section">
-                  <h3 className="point-title">{ title }</h3>
-                  <p className="point-context">{ context }</p>
-                </div>
-              </div>
-            );
-          }
-
-          align = 'timeline-point-right';
-
-          return (
-            <div key={id} className={`${align}`}>
-              <div className="title-context-section">
-                <h3 className="point-title">{ title }</h3>
-                <p className="point-context">{ context }</p>
-              </div>
-
-              <div className="bulletpoint-section">
-                <div className="bulletpoint" />
-              </div>
-
-              <div className="date-section">
-                <p className="point-date">{ date }</p>
-              </div>
-            </div>
-          );
-        })
+        }) => (
+          <section key={id} className={ styles['timeline__point'] }>
+            <section className={styles['timeline__content']}>
+              <time>{ date }</time>
+              <h3 className={ styles['timeline__title'] }>{ title }</h3>
+              <p className={ styles['timeline__context'] }>{ context }</p>
+            </section>
+            <div className={ styles['timeline__bulletpoint'] }></div>
+          </section>
+        ))
       }
-      <div className="timeline-div" />
     </section>
   );
 }
