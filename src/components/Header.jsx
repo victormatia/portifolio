5fffccec32b785bee9fe97b0720239cbcf817998
victormatia <fvictor1705@gmail.com>
2023-05-07 17:26:10 -0300

    feat: add show nav bar logic to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,31 @@
 import logo from '../images/VITU-logo.png';
 import styles from '../css/modules/Header.module.css'
 import Switch from '../components/Switch';
+import { useContext, useEffect, useRef } from 'react';
+import globalContext from '../context/globalContext';
 
 function Header() {
+  const { setIsNavBarActive } = useContext(globalContext)
+  const headerRef = useRef(null);
+
+  const options = {
+    root:  null,
+    rootMargin: '0px',
+    threshold: 1,
+  };
+
+  const controlNavBar = (entries) => {
+    const [entrie] = entries;
+
+    setIsNavBarActive(!entrie.isIntersecting);
+  }
+
+  const header = new IntersectionObserver(controlNavBar, options)
+
+  useEffect(() => { header.observe(headerRef.current) }, [headerRef])
+
   return (
-    <header className={ styles['header'] }>
+    <header ref={ headerRef } className={ styles['header'] }>
       <section className={ styles['header__l-side'] }>
         <img className={ styles['header__logo'] } src={ logo } alt="VITU logo" />
         <ul className={ styles['header__links'] }>
