69043e24db9aaa022a3d203016604c551076edf4
victormatia <fvictor1705@gmail.com>
2023-05-08 13:34:46 -0300

    feat: update text and spell checks

--- src/mock/mockApi.js
+++ src/mock/mockApi.js
@@ -35,18 +35,18 @@ export const projects = [
 ];
 
 export const aboutMe = {
-  about: 'Um pouco mais sobre mim. Tenho 24 anos e sou Cearense. Há um tempo, descobri minha paixão pelo desenvolvimento de software e, em 20 de abril de 2022, decidi entrar de cabeça nesse mundo e me tornar um dev. Atualmente, sou um profissional front end certificado, porém, sigo me especializando, e venho estudando tecnologias back end para assim me tornar um desenvolvedor full stack. Dito isso, separei alguns momentos importantes da minha carreira na linha do tempo a seguir:',
+  about: 'Um pouco mais sobre mim. Tenho 24 anos e sou Cearense. Há um tempo, descobri minha paixão pelo desenvolvimento de software e, em 20 de abril de 2022, decidi entrar de cabeça nesse mundo e me tornar um dev. Atualmente, sou um profissional full stack certificado pela Trybe, porém, sigo me especializando, e venho estudando python como minha segunda linguagem de programação. Dito isso, separei alguns momentos importantes da minha carreira na linha do tempo a seguir:',
   timeLinePoints: [
     {
       id: 1,
       title: '❤️ | Descobri minha paixão por desenvolvimento de softwares',
-      context: 'Precisei de um site para a minha empresa e descidi construir por conta própria',
+      context: 'Precisei de um site para a minha empresa e decidi construir um por conta própria',
       date: '2021/06',
     },
     {
       id: 2,
       title: '🎯 | Decidi me dedicar totalmente aos estudos de programação',
-      context: 'Optei por fazer transição de carreira  e iniciei o curso de desenvolvimento web na Trybe para me tornar um profissional Full Stack',
+      context: 'Optei por fazer transição de carreira  e iniciei o curso de desenvolvimento web na Trybe para me tornar um profissional Full Stack.',
       date: '2022/04',
     },
     {
@@ -58,14 +58,20 @@ export const aboutMe = {
     {
       id: 4,
       title: '🎨 | Me tornei um profissional Front End!',
-      context: 'Finalizei o segundo módulo da Trybe, ganhei a certificação e agora posso atuar no mercado como desenvolvedor front end',
+      context: 'Finalizei o segundo módulo da Trybe e, após conquistar 100% de aprovação em todos os projetos avaliativos, ganhei a certificação de profissional Front end.',
       date: '2022/10',
     },
     {
       id: 5,
-      title: '🚀 | Consegui meu primeiro estágio em tecnologia!',
-      context: 'Passei no processo seletivo da Trybe para estagiar como Student Summer Job de instrução',
+      title: '🚀 | Consegui meu primeiro emprego em tecnologia!',
+      context: 'Passei no processo seletivo da Trybe para trabalhar como Student Summer de instrução.',
       date: '2023/01',
+    },
+    {
+      id: 6,
+      title: '👨🏼‍💻 | Me tornei um profissional Full Stack!',
+      context: 'Finalizei o tercéiro módulo da Trybe e, após conquistar 100% de aprovação em todos os projetos avaliativos, ganhei a certificação de profissional Back end.',
+      date: '2022/10',
     },
   ],
 }
