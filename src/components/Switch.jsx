32faf23951b014601fd564a3ce58719e3b22230b
victormatia <fvictor1705@gmail.com>
2023-05-12 17:58:03 -0300

    feat: apply  switch language logic

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import styles from '../css/modules/Switch.module.css'
+import { useTranslation } from 'react-i18next';
 
 function Switch() {
+  const { i18n } = useTranslation()
   const [language, setLanguage] = useState(
     {
       pt: styles['switch__p__enabled'],
@@ -10,12 +12,27 @@ function Switch() {
     },
   );
 
+  // Da pra melhor essa lógica. language e currLanguage fazem coisas parecidas
+  const [_currLanguage, setCurrLanguage] = useState('ptBR')
+
+  const translate = () => {
+    setCurrLanguage((prev) => {
+      const curr = prev === 'ptBR' ? 'en' : 'ptBR'
+
+      i18n.changeLanguage(curr)
+      
+      return curr
+    })
+  }
+
   return (
     <section className={ styles['switch'] }>
       <p className={ language.pt }>PT-BR</p>
         <div
           className={ styles['switch__background'] }
           onClick={ () => {
+            translate()
+
             if (language.pt === styles['switch__p__enabled']) {
               setLanguage({
                  pt: styles['switch__p__disabled'],
