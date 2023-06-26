e8fa6dce62ca5151e2198cd385e2f30e3f336d72
victormatia <fvictor1705@gmail.com>
2023-06-26 06:20:18 -0300

    refactor: update contents of html tags

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Card from "../components/Card";
 import { projects } from "../mock/mockApi";
 import styles from '../css/modules/Projects.module.css'
+import { useTranslation } from "react-i18next";
 
 function Projects () {
+  const { t } = useTranslation();
   return (
     <section className={ styles['projects'] }>
       <section className={ styles['projects__content'] }>
-        <h1 className={ styles['projects__title'] }>Projetos</h1>
-        <p className={ styles['projects__paragraph'] }>Aqui estão alguns projetos que, durante minha jornada como desenvolvedor, me orgulho de ter feito ou participado</p>
+        <h1 className={ styles['projects__title'] }>{ t('projectsTitle') }</h1>
+        <p className={ styles['projects__paragraph'] }>{ t('projectsParagraph') }</p>
         <ul className={ styles['projects__list'] }>
           { projects.map(({ name, tag, thumb, repo, link }, i) => (
             <Card projectInfos={{ name, tag, thumb, repo, link }}  key={ i }/>
