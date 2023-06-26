03c1a847dbe96011fcf50b802445e9ce1d58b6ee
victormatia <fvictor1705@gmail.com>
2023-06-26 16:52:14 -0300

    refactor: import useTraslantion hook and update html tags content

--- src/components/LateralMenu.jsx
+++ src/components/LateralMenu.jsx
@@ -1,16 +1,19 @@
+import { useTranslation } from 'react-i18next';
 import Switch from '../components/Switch';
 import styles from '../css/modules/LateralMenu.module.css'
 import { IoMdClose } from 'react-icons/io'
 
 export default function LateralMenu({ setShowLMenu }) {
+  const { t } = useTranslation();
+
   return (
     <menu className={ styles['l-menu'] } onClick={() => setShowLMenu((prev) => !prev)}>
       <section className={ styles['l-menu__content'] }>
         <IoMdClose />
-        <a href="#home">home</a>
-        <a href="#about">sobre</a>
-        <a href="#projects">projetos</a>
-        <a href="#contact">contato</a>
+        <a href="#home">{ t('headerMenuItenHome') }</a>
+        <a href="#about">{ t('headerMenuItenAbout') }</a>
+        <a href="#projects">{ t('headerMenuItenProjects') }</a>
+        <a href="#contact">{ t('headerMenuItenContact') }</a>
         <row className={ styles['l-menu__row'] } />
         <Switch />
       </section>
