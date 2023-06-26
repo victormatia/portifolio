8952e290833f6bf96d26161eb2026115ca17e7b6
victormatia <fvictor1705@gmail.com>
2023-06-26 16:41:52 -0300

    refactor: import useTraslantion hook and update html tags content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import { DiGithubBadge } from 'react-icons/di'
 import { AiFillLinkedin } from 'react-icons/ai'
 import { HiOutlineMail } from 'react-icons/hi'
 import styles from '../css/modules/Contact.module.css'
+import { useTranslation } from 'react-i18next'
 
 const copyEmail = () => {
   const email = "victormatias.contato@gmail.com";
@@ -11,10 +12,12 @@ const copyEmail = () => {
 }
 
 function Contact () {
+  const { t } = useTranslation();
+
   return (
     <section className={ styles['contact'] }>
       <section className={ styles['contact__content'] }>
-        <h1 className={ styles['contact__title'] }>Contato</h1>
+        <h1 className={ styles['contact__title'] }>{ t('contactTitle') }</h1>
         <ul className={ styles['contact__list'] }>
           <a href="https://www.linkedin.com/in/-victormatias/" target="_blank">
             <li className={ `${styles['contact__item']} ${styles['contact__item__blue-bg']}` }>
@@ -33,7 +36,7 @@ function Contact () {
             <p>Email</p>
           </li>
         </ul>
-        <p className={ styles['contact__credits'] }>© Todos os direitos reservados | Um site desenvolvido por Victor Matias</p>
+        <p className={ styles['contact__credits'] }>{ t('contactCredits') }</p>
       </section>
     </section>
   );
